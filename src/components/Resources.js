f56d5a14624892f7120052aa96f81d6669d295b2
Manuel Perez <mperezy@unitru.edu.pe>
2018-09-18 15:40:00 -0500

    Using container tag insted of div tag

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Row, Col } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 
 const Resources = () => {
     return (
-        <div>
+        <Container>
             <Row>
                 <Col>
                     <h2>Check the <b><code>documentation</code></b>, are very useful!</h2>
@@ -12,7 +12,7 @@ const Resources = () => {
             <b><a target={"blank"} href={'https://reactjs.org/docs/getting-started.html'}>React documentation</a></b>
             <br/>
             <b><a target={"blank"} href={'https://reactstrap.github.io'}>Reactstrap documentation</a></b>
-        </div>
+        </Container>
     );
 };
 
